ccc851c592080757d26eb5ab35533733f0a2afb7
sdemirjian <sdemirjian@fwparker.org>
2017-03-31 14:44:56 -0500

    yee

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -5,3 +5,15 @@
 # Use BeautifulSoup and urllib to grab the text contents of the tweets
 # located on the twitter page you chose.  The .text attribute will supply the content of a soup object.
 # Have fun.  Again, nothing explicit. (15pts)
+import urllib.request
+from bs4 import BeautifulSoup
+
+url = "https://twitter.com/officialjaden?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
+
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page.read(), "html.parser")
+
+twitter_table = [x.text.strip() for x in soup.findAll("p", {"class":"TweetTextSize"})]
+print(twitter_table)
+for i in range(len(twitter_table)):
+    print(twitter_table[i])
--- weather_scrape.py
+++ weather_scrape.py
@@ -1,4 +1,3 @@
-
 # Below is a link to a 10-day weather forecast at weather.com
 # Use urllib and BeautifulSoup to scrape data from the weather table.
 # Print a brief synopsis of the weather for the next 10 days.
@@ -7,6 +6,36 @@
 # for the user.  You will need to target specific classes or other
 # attributes to pull some parts of the data.
 # (e.g.  Wednesday, March 22: the high temp will be 48 with a low of 35, and a 20% chance of rain). (25pts)
-
+import urllib.request
+from bs4 import BeautifulSoup
 
 url = "https://weather.com/weather/tenday/l/USIL0225:1:US"
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page.read(), "html.parser")
+
+#print(soup.prettify())
+
+weather_list = [[[z.text.strip() for z in y.findAll("span",{"class":"date-time"})] for y in x.findAll("td", {"class":"twc-sticky-col"})] for x in soup.findAll("table", {"class":"twc-table"})]
+#print(weather_list)
+
+
+better_list = []
+for i in range(len(weather_list[0])):
+    if (i+1)%2 == 0:
+        better_list.append(weather_list[0][i])
+better_list = [x[0] for x in better_list]
+#print(better_list)
+
+temp_list = [[[z.text.strip() for z in y.findAll("span")] for y in x.findAll("td", {"class":"temp"})] for x in soup.findAll("table", {"class":"twc-table"})]
+#print(temp_list)
+
+high_list = []
+low_list = []
+for i in range(len(temp_list[0])):
+    high_list.append(temp_list[0][i][0])
+    low_list.append(temp_list[0][i][2])
+#print(high_list)
+#print(low_list)
+
+for i in range(len(better_list)):
+    print("The forecast for", better_list[i], "is a high of", high_list[i], "and a low of", low_list[i])
